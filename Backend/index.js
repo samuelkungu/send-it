a6fc3f065f31d6d391edc9125bd69598fa1a2293
Samuel <kungusamuel64@gmail.com>
2022-03-08 11:11:47 +0300

    Endpoints Configuration

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express ()
 
 app.use(express.json())
-app.use('/users', require('./Routes/userRoutes'))
+app.use('/Parcel-Delivery', require('./Routes/userRoutes'))
 app.listen(3005, ()=> {
     console.log("Server Is Running on port 3005")
 })
