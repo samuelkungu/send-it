9dab7e7f7cfe4ff4dc4c640d2e168970b9084d1c
Samuel <kungusamuel64@gmail.com>
2022-03-09 11:00:47 +0300

    Helpers

--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,8 @@ const express = require('express')
 const app = express ()
 
 app.use(express.json())
-
 // -------------- CONNECTING TO USERS ROUTES ------------------
 app.use('/users', require('./Routes/userRoutes'))
-
 // -------------- CONNECTING TO PARCELS ROUTES ------------------
 app.use('/parcels', require('./Routes/parcelRoutes'))
 
