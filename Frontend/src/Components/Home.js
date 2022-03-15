6252a42304a155db6ddd8b9237a7355908d24a69
Samuel <kungusamuel64@gmail.com>
2022-03-15 12:32:40 +0300

    Services And Parcel Tracking

--- Frontend/src/Components/Home.js
+++ Frontend/src/Components/Home.js
@@ -25,20 +25,27 @@ function Home() {
           </div>
         </div>
 
-        <div className='card mt-4 pt-2 '>
-          <div className='card-body'>
-                    <div>
-                        <p className=''>Collection From</p> 
-                  
+        <div className='card mt-4 pt-2 col-10 d-flex flex-column'>
+          <div className='card-body d-flex flex-row '>
+                    <div className='col-5'>
+                        <p className=''>Collection From</p>               
                         <input type=" text" className='' placeholder='Area'/>
                         <input type=" text" className='ms-2' placeholder='Address'/>
                     </div>
-                    <div>
+                    <div className='col-5'>
                         <p className=''>Delivery To</p> 
-                  
                         <input type=" text" className='' placeholder='Area'/>
                         <input type=" text" className='ms-2' placeholder='Address'/>
                     </div>
+          </div>
+          <div className='card-body d-flex flex-row'>
+                  <div className='col-5'>
+                    <p>Package</p>
+                    <button className='btn btn-outline-secondary  p-2 ms-2'>Envelope</button>
+                    <button className='btn btn-outline-secondary  p-2 ms-2'>Box</button>
+                    <button className='btn btn-outline-secondary  p-2 ms-2'>Documents</button>
+                    <button className='btn btn-outline-secondary  p-2 ms-2'>Pallet</button>
+                  </div>
           </div>
         </div>
 
