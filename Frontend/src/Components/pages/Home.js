c4909a831b2bb6083de0d59f79d4da47fabc82c5
Samuel <kungusamuel64@gmail.com>
2022-03-17 13:07:44 +0300

    Integrating images

--- Frontend/src/Components/pages/Home.js
+++ Frontend/src/Components/pages/Home.js
@@ -2,26 +2,32 @@ import React from 'react'
 import Carousel from './Carousel';
 
 function Home() {
-  return (<div>
+  return (<div className='home-page '>
   <div class="home pt-2 container-fluid">
  
-    <div className='card mt-4 mx-4  pt-4 '>
-      <div className='card-body d-flex justify-content-between flex-row'>
+    <div className=' card mt-4 mx-4  pt-4  '>
+              <div className='card-body d-flex flex-row justify-content-between'>
 
-              <div className=''>
-                <h3> Quickest and Safest Delivery</h3>
-                <p> We guarantee fast and safe transport for your package</p>
-                <p> If you want to track all of your deliveries, without wasting time checking websites 
-                <br/>  Parcel is the right app for you.
-                <br/>This app will track your deliveries and do even more.</p>
-              </div>
+                        <div className='d-flex'>
+                    
+                          <div className='align-self-start'>
+                            <h3> Quickest and Safest Delivery</h3>
+                          </div>
+                      
+                          <div className='align-self-end'>
+                            <p> We guarantee fast and safe transport for your package</p>
+                              <p> If you want to track all of your deliveries, without wasting time checking websites 
+                              <br/>  Parcel is the right app for you.
+                              <br/>This app will track your deliveries and do even more.</p>
+                          </div>
 
-             
+                        </div>
                 
-              <div className='col-md-4 px-0'>
-                <img  className='img-fluid rounded-circle' src='/images/parcel1.jpg ' alt='logo'/>
-               </div>
-      </div>
+                <div className='col-md-4 px-0'>
+                      <img  className='img-fluid rounded-circle' src='/images/parcel1.jpg ' alt='logo'/>
+                </div>
+
+              </div>
     </div>
 
     
