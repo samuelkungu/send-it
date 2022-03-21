430d3309666b4e23c8d4b19162307ddbd9cf42dd
Samuel <kungusamuel64@gmail.com>
2022-03-21 15:07:18 +0300

    Login useState

--- Frontend/src/Components/Views/LoginView.js
+++ Frontend/src/Components/Views/LoginView.js
@@ -10,49 +10,55 @@ function LoginView() {
     return email.length >0 && password.length >0;
   }
 
+ // Handling the email change
+ const handleEmail = (e) => {
+  setEmail(e.target.value);
+  // setSubmitted(false);
+};
 
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
+// Handling the password change
+const handlePassword = (e) => {
+  setPassword(e.target.value);
+  // setSubmitted(false);
+};
 
-    return (<div className='page '>
-      
+    return (
+    <div className='page '>
     <div className="wrapper">
     <div className="inner">
-   
     <div className="Login">
+
     <form>
-    <div className="form-group p-2 ">
+      <div className="form-group p-2 ">
         <label>Email address</label>
         <input type="email"  className="form-control" placeholder="Enter email"  name="email"
-        autoFocus   value={email}  onChange={(e) => setEmail(e.target.value)} />
-        
-    </div>
+        autoFocus   onChange={handleEmail} value={email}/>
+      </div>
 
-    <div className="form-group p-2 ">
+      <div className="form-group p-2 ">
         <label>Password</label>
         <input  type="password" className="form-control " placeholder="Enter password" name="password" 
-        value={password}  onChange={(event) => setPassword(event.target.value)} />
-    </div>
+         onChange={handlePassword} value={password} />
+      </div>
 
-    <div className="form-group p-2 ">
+      <div className="form-group p-2 ">
                     <input type="checkbox" className="custom-control-input" id="customCheck1" />
                     <label>Remember me</label>
-    </div>
+      </div>
 
-        <button button type="submit" className="btn btn-primary  btn-block "
+      <button button type="submit" className="btn btn-primary btn-block"
         onClick={handleSubmit} disabled={!validateForm()}> Login </button>
-        <p className="forgot-password text-right">
-                You don't have an account?<a href="/RegisterPage">Register?</a>
-            </p>
+        <p className="forgot-password text-right"> You don't have an account?
+          <a href="/RegisterPage">Register?</a>  
+        </p>
 
         
     </form>
-  </div>
-  </div>
-</div>
 
-</div>)
+    </div>
+    </div>
+    </div>
+    </div>)
 }
 
 
