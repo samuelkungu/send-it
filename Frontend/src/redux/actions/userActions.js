359e5f7105468633eaca222e42e7a2fb2176abc1
Samuel <kungusamuel64@gmail.com>
2022-03-22 14:58:45 +0300

    Axios integration

--- Frontend/src/redux/actions/userActions.js
+++ Frontend/src/redux/actions/userActions.js
@@ -1,11 +1,5 @@
 import axios from "axios"
 import {
-  USER_DETAILS_FAIL,
-  USER_DETAILS_REQUEST,
-  USER_DETAILS_SUCCESS,
-  USER_LIST_FAIL,
-  USER_LIST_REQUEST,
-  USER_LIST_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
@@ -13,9 +7,7 @@ import {
   USER_REGISTER_FAIL,
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
-  USER_UPDATE_PROFILE_FAIL,
-  USER_UPDATE_PROFILE_REQUEST,
-  USER_UPDATE_PROFILE_SUCCESS,
+ 
 } from "../constants/userConstants"
 
 //sign in user to the app
@@ -31,11 +23,7 @@ export const login = (email, password) => async (dispatch) => {
       },
     }
 
-    const { data } = await axios.post(
-      "/api/users/login",
-      { email, password },
-      config
-    )
+    const { data } = await axios.post( "/api/users/login",{ email, password },config )
 
     dispatch({
       type: USER_LOGIN_SUCCESS,
@@ -94,98 +82,6 @@ export const signup = (name, email, password, role) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: USER_REGISTER_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    })
-  }
-}
-
-//get user details, this action will be used for getting the logged user profile
-// & also for getting a user by id (to not duplicate the code)
-//so the id will be the id of the user if we want a user by id & if we wan logge user profile then id==profile
-//getState => to get the token
-export const getUserDetails = (id) => async (dispatch, getState) => {
-  try {
-    dispatch({
-      type: USER_DETAILS_REQUEST,
-    })
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${getState().userLogin.userInfo.token}`,
-      },
-    }
-
-    const { data } = await axios.get(`/api/users/${id}`, config)
-
-    dispatch({
-      type: USER_DETAILS_SUCCESS,
-      payload: data,
-    })
-  } catch (error) {
-    dispatch({
-      type: USER_DETAILS_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    })
-  }
-}
-
-//Update User Profile
-export const updateUserProfile = (user) => async (dispatch, getState) => {
-  try {
-    dispatch({
-      type: USER_UPDATE_PROFILE_REQUEST,
-    })
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${getState().userLogin.userInfo.token}`,
-      },
-    }
-
-    const { data } = await axios.put("/api/users/profile", user, config)
-
-    dispatch({
-      type: USER_UPDATE_PROFILE_SUCCESS,
-      payload: data,
-    })
-  } catch (error) {
-    dispatch({
-      type: USER_UPDATE_PROFILE_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    })
-  }
-}
-
-//Get users list
-export const usersList = () => async (dispatch, getState) => {
-  try {
-    dispatch({
-      type: USER_LIST_REQUEST,
-    })
-
-    const config = {
-      headers: {
-        Authorization: `Bearer ${getState().userLogin.userInfo.token}`,
-      },
-    }
-    const { data } = await axios.get("/api/users", config)
-
-    dispatch({
-      type: USER_LIST_SUCCESS,
-      payload: data,
-    })
-  } catch (error) {
-    dispatch({
-      type: USER_LIST_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,14 +1,28 @@
-import { combineReducers, createStore } from "redux"
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly"
-import userReducer from "./reducers/userReducer";
+import { createStore, combineReducers, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
+import { composeWithDevTools } from "redux-devtools-extension"
+import {userLoginReducer,userSignUpReducer }from "./reducers/userReducer";
 import parcelReducer from "./reducers/parcelReducer";
 
+
 const rootReducer = combineReducers({
-        user: userReducer,
+        userLogin: userLoginReducer,
+        userRegister: userSignUpReducer,
         parcel: parcelReducer,
 
 });
+const userInfoFromLocalStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null
 
-const store = createStore(rootReducer, composeWithDevTools());
+const initialState = {
+        userLogin: { userInfo: userInfoFromLocalStorage },
+        },
+      }
 
-export default store;
+const middleware = [thunk]
+
+const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store
