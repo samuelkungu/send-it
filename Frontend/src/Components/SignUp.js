3183219752481abe8f5207210875280a62c88ea5
Samuel <kungusamuel64@gmail.com>
2022-03-23 11:35:52 +0300

    Login Validation

--- Frontend/src/Components/SignUp.js
+++ Frontend/src/Components/SignUp.js
@@ -4,26 +4,30 @@ import { signup } from "../redux/actions/userActions"
 
 function SignUp() {
 
-  const [userName, setUserName] = useState("")
-  const [fullName, setFullName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [confirmPassword, setConfirmPassword] = useState("")
-  const [passwordsNotMatch, setPasswordsNotMatch] = useState(null)
+  const [userData, setUserData] = useState({
+    username : " ",
+    fullName : " ",
+    email : " ",
+    password : " ",
+    username : " ",
+    confirmPassword : " ",
+  });
+  
 
   // const { loading, error, userInfo } = useSelector((state) => state.useRegister)
 
-//   const dispatch = useDispatch();
-  const dispatch = email;
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    if (password !== confirmPassword) {
-      setPasswordsNotMatch("Passwords Not Match!")
-    } else {
-      
-      dispatch(signup(userName, fullName, email, password))
-    }
-  } 
+  //   const dispatch = useDispatch();
+  const handleChange = ( event ) =>{
+      setUserData({
+        ...userData , 
+        [event.target.name]: event.target.userData,
+      });
+    };
+
+  const [errors, setErrors] = useState({});
+  const  handleFormSubmit = ( event ) => {
+    event.preventDefault();
+  };
 
 
      
@@ -41,30 +45,30 @@ function SignUp() {
                 <div className="form-group p-2">
                     <label>Username</label>
                     <input type="text" className="form-control" placeholder="Username"
-                     value={userName} onChange={(e) => setUserName(e.target.value)}/>
+                     value={userData.userName} onChange={handleChange}/>
                 </div>
                 <div className="form-group p-2">
                     <label>Full name</label>
                     <input type="text" className="form-control" placeholder="Full name"
-                     value={fullName} onChange={(e) => setFullName(e.target.value)} />
+                     value={userData.fullName} onChange={handleChange} />
                 </div>
                 <div className="form-group p-2">
                     <label>Email address</label>
                     <input type="email" className="form-control" placeholder="Enter email"
-                      value={email} onChange={(e) => setEmail(e.target.value)} />
+                      value={userData.email}  onChange={handleChange} />
                 </div>
                 <div className="form-group p-2">
                     <label>Password</label>
                     <input type="password" className="form-control" placeholder="Enter password"  
-                     value={password} onChange={(e) => setPassword(e.target.value)} />
+                     value={userData.password} onChange={handleChange} />
                 </div>
                 <div className="form-group p-2">
                     <label>Confirm Password</label>
                     <input type="password" className="form-control" placeholder="Confirm Password"  
-                     value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                     value={userData.confirmPassword} onChange={handleChange} />
                 </div>
 
-                <button type="submit" className="btn btn-primary btn-block mt-1" onSubmit={handleSubmit} >
+                <button type="submit" className="btn btn-primary btn-block mt-1" onSubmit={handleFormSubmit} >
                     Sign Up</button>
                 <p className="forgot-password text-right">
                     You already have an account?.
